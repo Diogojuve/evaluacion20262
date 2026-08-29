adf96724d80e2da32ea4851eaa8efd7be6b10691
Diogojuve <diogojuvenalticona@gmail.com>
2026-08-28 21:40:59 -0500

    feat: implementar listado de solicitudes

--- Controllers/SolicitudController.cs
+++ Controllers/SolicitudController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using TecnoGasHogar.Data;
 using TecnoGasHogar.Models;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace TecnoGasHogar.Controllers
 {
@@ -13,6 +14,18 @@ namespace TecnoGasHogar.Controllers
         public SolicitudController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        // GET: Solicitud (Listado de solicitudes)
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            // Consulta EF Core ordenando de la más reciente a la más antigua
+            var lista = await _context.SolicitudesServicio
+                .OrderByDescending(s => s.FechaRegistro)
+                .ToListAsync();
+
+            return View(lista);
         }
 
         // GET: Solicitud/Registrar
