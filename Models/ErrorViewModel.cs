2f32242d92580d24065a82f0011931288cd35f35
Diogojuve <diogojuvenalticona@gmail.com>
2026-08-28 21:21:06 -0500

    chore: estructura inicial del proyecto .NET 10 MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Evaluacion_2026_2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
